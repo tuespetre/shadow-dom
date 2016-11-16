0bae98cebf5554602fde30406fa4a4b9c545783d
Derek Gray <dgray90@live.com>
2016-11-16 07:59:01 -0600

    quote forceShadowDomPolyfill check on window

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import patch from './patch.js';
 
 const nativeShadowDom = 'attachShadow' in Element.prototype;
 
-if (!nativeShadowDom || window.forceShadowDomPolyfill) {
+if (!nativeShadowDom || window['forceShadowDomPolyfill']) {
     patch();
 }
